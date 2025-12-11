f8c6738905b06050b30b7398ef9eefbe71ba8ad8
jappsb27 <jappsb27@scs.local>
2025-12-11 01:51:48 -0800

    Commit

--- src/app/calculator/calculator.ts
+++ src/app/calculator/calculator.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  //imports: [],
   templateUrl: './calculator.html',
   styleUrls: ['./calculator.css'],
 })
